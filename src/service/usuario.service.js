1b71bdeda5d06a1895b68049d7770ba1359d5583
AlonsoFavero <alonso.favero@gmail.com>
2026-05-29 11:08:44 -0300

    refactor: melhorar retorno do usuário removendo senha

--- src/service/usuario.service.js
+++ src/service/usuario.service.js
@@ -9,9 +9,15 @@ if(usuarioExiste){
     throw new AppError("email ja cadastrado", 409)
 }
 const senhaHash = await bcrypt.hash(senha,10)
-const novoUsuario = await Usuario.create({
+const novoUsuario = await usuario.create({
     nome,
     email,
     senha: senhaHash
 })
+
+const usuarioSemSenha = novoUsuario.toObject()
+
+delete usuarioSemSenha.senha
+
+return usuarioSemSenha
 }
