b6f470b5805a18a625d63f36486124f61548d74a
AlonsoFavero <alonso.favero@gmail.com>
2026-05-29 09:31:54 -0300

    feat: criar model de usuário com timestamps

---
+++ src/models/usuario.model.js
@@ -0,0 +1,31 @@
+const mongoose = require ("mongoose")
+
+const usuarioSchema = new mongoose.Schema({
+    nome: {
+   type: String,
+   required: true
+    },
+
+    email: {
+     type: String,
+     required: true,
+     unique: true
+    },
+
+    senha: {
+     type: String,
+     required: true,
+     select: false
+    },
+
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    }
+},
+{
+    timestamps: true
+})
+
+module.exports = mongoose.model("Usuario", usuarioSchema)
