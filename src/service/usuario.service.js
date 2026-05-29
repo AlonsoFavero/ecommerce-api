3e1d6354593770ccd232644f41a46c3a5e38d899
AlonsoFavero <alonso.favero@gmail.com>
2026-05-29 10:55:48 -0300

    feat: iniciar service de criação de usuário com validação de email

--- src/service/usuario.service.js
+++ src/service/usuario.service.js
@@ -1,7 +1,17 @@
 const bcrypt = require("bcrypt");
-const usuario = require("../models/usuario.model")
+const usuario = require("../models/Usuario.model")
 const AppError = require("../utils/AppError")
 
 async function criar(nome,email,senha){
+const usuarioExiste = await usuario.findOne({email})
 
+if(usuarioExiste){
+    throw new AppError("email ja cadastrado", 409)
+}
+const senhaHash = await bcrypt.hash(senha,10)
+const novoUsuario = await Usuario.create({
+    nome,
+    email,
+    senha: senhaHash
+})
 }
