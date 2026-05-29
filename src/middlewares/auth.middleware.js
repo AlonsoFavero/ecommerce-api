0624c2d2e342064bfee9ca6b0e825fe6bcd3fb85
AlonsoFavero <alonso.favero@gmail.com>
2026-05-29 12:57:02 -0300

    feat: implementar authMiddleware para proteção de rotas

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken")
+
+function authMiddleware(req,res,next){
+const auth =  req.headers.authorization
+
+if(!auth){
+    return res.status(401).json({ erro: "token não enviado"})
+}
+
+const partes = auth.split(" ")
+
+if (partes.length !== 2){
+    return res.status(401).json({erro: "token mal formatado"})
+}
+
+const token = partes[1]
+
+try{
+const decoded = jwt.verify(
+    token,
+    process.env.JWT_SECRET)
+
+req.user = decoded
+
+next()
+    }catch(error){
+   return res.status(401).json({"error": "tokem inválido"})
+ }
+
+}
