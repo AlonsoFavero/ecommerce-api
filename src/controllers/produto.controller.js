cc367e6e5b27fa805dfacc98fafa35d1b17dd13b
AlonsoFavero <alonso.favero@gmail.com>
2026-05-30 03:40:50 -0300

    feat: implementar CRUD completo de produtos no controller

--- src/controllers/produto.controller.js
+++ src/controllers/produto.controller.js
@@ -15,6 +15,62 @@ const criarProduto = async(req,res) => {
     })
 }
 
+const listarProduto = async(req,res) => {
+
+    const produto = await produtoService.listar()
+
+    return res.status(200).json({
+   mensagem: "produtos listados com sucesso",
+   produto
+    })
+}
+
+const deletarProduto = async(req,res) => {
+
+    req.params.id 
+
+    const produto = await produtoService.deletar(id)
+
+    return res.status(200).json({
+        mensagem: "produto deletado com sucesso",
+        produto
+    })
+
+}
+
+const buscarProduto = async(req,res) => {
+    const productId = req.params.id
+    const produto = await produtoService.buscarPorId(productId)
+
+    return res.status(200).json({
+        mensagem: "produto encontrado com sucesso",
+        produto
+    })
+}
+
+const atualizarProduto = async(req,res) => {
+    const {nome,preco,descricao,estoque} = req.body
+    const productId = req.params.id
+
+    const produto = await produtoService.atualizar(
+        productId,{
+            nome,
+            preco,
+            descricao,
+            estoque
+        }
+    )
+
+    return res.status(200).json({
+        mensagem: "produto atualizado com sucesso",
+        produto
+    })
+}
+
 module.exports = {
-    criarProduto
+    criarProduto,
+    listarProduto,
+    deletarProduto,
+    buscarProduto,
+    atualizarProduto
 }
