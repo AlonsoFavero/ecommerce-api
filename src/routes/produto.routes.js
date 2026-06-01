b5d8c0f3230145a691140ac43f4b002f21e47829
AlonsoFavero <alonso.favero@gmail.com>
2026-06-01 15:59:36 -0300

    feat: implementar rotas REST completas de produtos

--- src/routes/produto.routes.js
+++ src/routes/produto.routes.js
@@ -1,9 +1,33 @@
 const {Router} = require("express")
-const{criarProduto} = require("../controllers/produto.controller")
+const{criarProduto, listarProduto, buscarProduto, deletarProduto, atualizarProduto} = require("../controllers/produto.controller")
 const {authMiddleware} = require("../middlewares/auth.middleware")
 const {adminMiddleware} = require("../middlewares/admin.middleware")
 
 const router = Router()
+
+router.get(
+    "/:id",
+    buscarProduto
+)
+
+router.get(
+    "/",
+    listarProduto
+)
+
+router.delete(
+    "/:id",
+    authMiddleware,
+    adminMiddleware,
+    deletarProduto
+)
+
+router.put(
+    "/:id",
+    authMiddleware,
+    adminMiddleware,
+    atualizarProduto
+)
 
 router.post("/", authMiddleware, adminMiddleware , criarProduto)
 
