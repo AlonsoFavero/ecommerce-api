9b8708768725cea02c76bd4c573e7cba390efad2
AlonsoFavero <alonso.favero@gmail.com>
2026-06-01 15:40:40 -0300

    feat: proteger rotas de produtos com JWT e autorização admin

--- src/routes/produto.routes.js
+++ src/routes/produto.routes.js
@@ -1,8 +1,10 @@
 const {Router} = require("express")
 const{criarProduto} = require("../controllers/produto.controller")
+const {authMiddleware} = require("../middlewares/auth.middleware")
+const {adminMiddleware} = require("../middlewares/admin.middleware")
 
 const router = Router()
 
-router.post("/",criarProduto)
+router.post("/", authMiddleware, adminMiddleware , criarProduto)
 
 module.exports = router
