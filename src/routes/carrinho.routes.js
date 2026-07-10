35306df6b9bebbdb6ec430d37b690ee0b27b9bcc
AlonsoFavero <alonso.favero@gmail.com>
2026-07-10 19:10:05 -0300

    refactor: remove old pedido controller and clean routes

--- src/routes/carrinho.routes.js
+++ src/routes/carrinho.routes.js
@@ -1,16 +1,8 @@
 const {Router} = require("express")
 const authMiddleware = require("../middlewares/auth.middleware")
 const{finalizarCompraController} = require("../controllers/carrinho.controller")
-const{listarPedidosController} = require("../controllers/pedido.controller")
 
 const router = Router()
-
-router.get(
-"/",
-authMiddleware,
-listarPedidosController
-)
-
 
 router.post("/", authMiddleware, finalizarCompraController)
 
