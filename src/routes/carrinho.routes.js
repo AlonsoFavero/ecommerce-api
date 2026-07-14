c870dad630eee3ce2fd317ba7d668abf1cf6e03b
AlonsoFavero <alonso.favero@gmail.com>
2026-07-14 19:07:27 -0300

    fix: corrige rotas do carrinho para fluxo de compra

--- src/routes/carrinho.routes.js
+++ src/routes/carrinho.routes.js
@@ -1,9 +1,11 @@
 const {Router} = require("express")
 const authMiddleware = require("../middlewares/auth.middleware")
-const{finalizarCompraController} = require("../controllers/carrinho.controller")
+const{finalizarCompraController,adicionarProdutoController} = require("../controllers/carrinho.controller")
 
 const router = Router()
 
-router.post("/", authMiddleware, finalizarCompraController)
+router.post("/", authMiddleware, adicionarProdutoController)
+
+router.post("/finalizar", authMiddleware, finalizarCompraController)
 
 module.exports = router
